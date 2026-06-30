1cb12e12b8c27d7640e1d7cf8a94cac380ea9024
lucaspinto2k <lucasreis@gmail.com>
2026-06-30 17:47:27 -0300

    Fix unused imports to pass Vercel build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
 "use client";
-import { useEffect } from "react";
+
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import CaseStudies from "@/components/CaseStudies";
-import Skills from "@/components/Skills";
-import Experience from "@/components/Experience";
-import Education from "@/components/Education";
-import Multimedia from "@/components/Multimedia";
-import Localization from "@/components/Localization";
-import Blogs from "@/components/Blogs";
-import Podcasts from "@/components/Podcasts";
-import Resume from "@/components/Resume";
+
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
 
